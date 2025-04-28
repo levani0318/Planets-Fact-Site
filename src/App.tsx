cb223c6a1f8dae1dc98139dc1cc9ddc6b0a1989e
levani0318 <levani0318@gmail.com>
2025-04-28 22:40:17 +0400

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import "./index.css";
 import Mercury from "./assets/MercuryFirst.png";
-import bgImage from "./assets/bg.png";
 
 function App() {
   return (
     <div className="">
-      <div className="w-full bg-[#070724] flex justify-around content-center gap-[594px] px-8 pt-[22px] text-[#FFF] border-b-white">
+      <div className="w-full flex justify-around content-center gap-[594px] px-8 pt-[22px] text-[#FFF] bg-[#070724] border-b-white">
         <h1 className="font-Antonio font-normal text-[28px] ">THE PLANETS</h1>
         <div className="font-Spartan">
           <Navigation />
         </div>
       </div>
-      <div className="w-full flex justify-around gap-[300px] px-[165px] pt-[126px] bg-[url('./assets/bg.png')] text-[#fff]">
+      <div className="w-full flex justify-around gap-[300px] px-[165px] pt-[126px] rounded-lg text-[#fff] bg-[#070724]">
         <div className="">
           <img
             className="w-[290px] h-[290px] ml-[170px] mr-[300px] mt-[116px]"
@@ -52,7 +51,7 @@ function App() {
         </div>
       </div>
       <div className="flex justify-around content-center gap-[30px] px-[135px] bg-[#070724] text-[#fff] pt-[87px] pb-[56px] uppercase">
-        <div className="w-[255px] border-[1px] --tw-border-opacity-20 border-white pl-[23px] py-[20px]">
+        <div className="w-[255px] border-[1px] border-white/20 pl-[23px] py-[20px]">
           <h5 className="upercase -Spartan font-bold text-[11px] leading-[25px] tracking-[1px] opacity-50">
             rotation time
           </h5>
@@ -60,7 +59,7 @@ function App() {
             58.6 days
           </h3>
         </div>
-        <div className="upercase w-[255px] border-[1px] --tw-border-opacity-20 border-white pl-[23px] py-[20px]">
+        <div className="upercase w-[255px] border-[1px] border-white/20 pl-[23px] py-[20px]">
           <h5 className="upercase font-Spartan font-bold text-[11px] leading-[25px] tracking-[1px] opacity-50">
             revolution time
           </h5>
@@ -68,7 +67,7 @@ function App() {
             87.97days
           </h3>
         </div>
-        <div className="w-[255px] border-[1px] --tw-border-opacity-20 border-white pl-[23px] py-[20px]">
+        <div className="w-[255px] border-[1px] border-white/20 pl-[23px] py-[20px]">
           <h5 className="upercase font-Spartan font-bold text-[11px] leading-[25px] tracking-[1px] opacity-50">
             radius
           </h5>
@@ -76,7 +75,7 @@ function App() {
             2,439.7km
           </h3>
         </div>
-        <div className="uppercase w-[255px] border-[1px] --tw-border-opacity-20 border-white pl-[23px] py-[20px]">
+        <div className="uppercase w-[255px] border-[1px] border-white/20 pl-[23px] py-[20px]">
           <h5 className="font-Spartan font-bold text-[11px] leading-[25px] tracking-[1px] opacity-50">
             average temp
           </h5>
